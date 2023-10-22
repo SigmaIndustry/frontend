d1c55eab8bb0f4c5ebdbf10b1bff8ea11a8157aa
kolasyik <mykola.primachenko@gmail.com>
2023-10-22 20:16:03 +0000

    fix classic-textArea.jsx

--- src/shared/inputs/classic-textarea/ClassicTextArea.tsx
+++ src/shared/inputs/classic-textarea/ClassicTextArea.tsx
@@ -2,13 +2,18 @@
 import React, {useEffect, useMemo, useState} from 'react';
 import styles from './classic-textarea.module.scss';
 import {TextArea} from "@radix-ui/themes";
-
+type ClassicTextAreaProps = {
+    children: any,
+    minHeight: number,
+    value: any,
+    setValue: (value: any) => void,
+}
 const ClassicTextArea = ({
                              children,
                              minHeight,
                              value,
                              setValue,
-                         }) => {
+                         }: ClassicTextAreaProps) => {
     return (
         <div
             className={styles.textarea__wrapper}
